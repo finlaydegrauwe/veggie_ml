1646aa237814b10c3da718143c93e4959b160063
finlaydegrauwe <finlaydegrauwe@gmail.com>
2022-01-21 14:43:45 +0100

    groentjes

--- src/components/groentesaver.jsx
+++ src/components/groentesaver.jsx
@@ -1,39 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "./button";
 
-const strings = [
+const stringsBegin = [
   "wortel",
   "knol",
   "bei",
   "bes",
   "peer",
   "spaghetti",
-  "raap"
+  "raap",
+];
+
+const stringsEnd = [
+  "wortel",
+  "knol",
+  "bei",
+  "bes",
+  "peer",
+  "raap",
 ];
 
 export default function GroenteSaver() {
   const [groenteNaam, setgroenteNaam] = useState(willeKeurigeGroente());
 
   function willeKeurigeGroente() {
-      return strings[Math.floor(Math.random() * strings.length)]+strings[Math.floor(Math.random() * strings.length)]+strings[Math.floor(Math.random() * strings.length)];
-    }
+    let groentje =
+      stringsBegin[Math.floor(Math.random() * stringsBegin.length)] +
+      stringsBegin[Math.floor(Math.random() * stringsBegin.length)] +
+      stringsEnd[Math.floor(Math.random() * stringsEnd.length)];
+    groentje = groentje.charAt(0).toUpperCase() + groentje.slice(1);
+    return groentje;
+  }
 
   return (
     <div className="groentesaver">
       <div className="flex-row">
-      <Button
-      willeKeurigeGroente={willeKeurigeGroente}
-      setgroenteNaam={setgroenteNaam}
-        title="andere!  "
-        icon="autorenew"
-        color="purple"
-      />
-      <p></p>
-      <Button title={groenteNaam} icon="save" color="blue" />
+        <Button
+          willeKeurigeGroente={willeKeurigeGroente}
+          setgroenteNaam={setgroenteNaam}
+          title="andere!  "
+          icon="autorenew"
+          color="purple"
+        />
+        <p></p>
+        <Button title={groenteNaam} icon="save" color="blue" />
       </div>
-        <p>Of:</p>
+      <p>Of:</p>
       <div className="flex-row">
-
         <input type="text" name="name" />
         <Button title="Opslaan" icon="save" color="blue" />
       </div>
